fb8fa2c21abf19353d4aa101344a2927bb7b5860
Aadit Kamat <plunges_abiotic.0q@icloud.com>
2025-04-14 10:53:56 -0400

    feat(home): Add searchbox at the top of the home page

--- frontend/src/app/(protected)/(tabs)/(home)/index.jsx
+++ frontend/src/app/(protected)/(tabs)/(home)/index.jsx
@@ -1,3 +1,18 @@
+import { Dimensions, Button } from "react-native";
+import React, { useState, useEffect, useRef, Easing } from "react";
+import {
+  View,
+  Text,
+  Image,
+  TextInput,
+  TouchableOpacity,
+  Alert,
+  StyleSheet,
+  ScrollView,
+  Animated,
+  Pressable,
+  Modal,
+} from "react-native";
 import { BlurView } from "@react-native-community/blur";
 import { useRouter } from "expo-router";
 import React, { useEffect, useRef, useState } from "react";
@@ -391,6 +406,302 @@ export default function Home() {
           <Text style={styles.calendarText}>Tap to View Calendar</Text>
         </TouchableOpacity>
       </View>
+<<<<<<< HEAD:frontend/src/app/(protected)/(tabs)/(home)/index.jsx
+=======
+
+      {/* Calendar Modal */}
+      <Modal
+        visible={isCalendarVisible}
+        transparent={true}
+        animationType="fade"
+      >
+        {/* Blur Effect */}
+        <BlurView
+          style={styles.blurView}
+          blurType="light"
+          blurAmount={isEventModalVisible ? 20 : 10} // Increase blur if Add Event modal is active
+        />
+
+        {/* Green Overlay */}
+        <View style={styles.greenOverlay} />
+
+        {/* Modal Content */}
+        <View style={styles.modalContentCalendar}>
+          <Text style={styles.modalTitleCalendar}>Community Calendar</Text>
+
+          {/* Calendar Component */}
+          <Calendar
+            markedDates={Object.keys(displayEvents).reduce((acc, date) => {
+              acc[date] = { marked: true };
+              return acc;
+            }, {})}
+            onDayPress={(day) => {
+              const events = displayEvents[day.dateString] || [];
+              if (events.length > 0) {
+                setSelectedEventDetails(events[0]); // Set the first event (assuming one per day)
+                setIsEventDetailModalVisible(true); // Trigger the modal to open
+              } else {
+                Alert.alert("No Events", "No events on this day.");
+              }
+            }}
+          />
+
+          {/* Close Button */}
+          <TouchableOpacity
+            style={styles.CalendarcloseButton}
+            onPress={toggleCalendar}
+          >
+            <Text style={styles.CalendarcloseButtonText}>X</Text>
+          </TouchableOpacity>
+
+          {/* Add Event Button */}
+          <TouchableOpacity
+            style={styles.calendarButton}
+            onPress={() => setIsEventModalVisible(true)} // Open Add Event Modal
+          >
+            <Text style={styles.calendarButtonText}>Add Event</Text>
+          </TouchableOpacity>
+        </View>
+
+        <Modal
+          visible={isEventModalVisible}
+          transparent={true}
+          animationType="slide"
+        >
+          <BlurView style={styles.blurView} blurType="light" blurAmount={30} />
+
+          <View style={styles.CalendarmodalBackground}>
+            <View style={styles.CalendareventModalContent}>
+              {/* Wrap all text inside <Text> */}
+              <Text style={styles.CalendarmodalTitle}>Add Event</Text>
+              <TextInput
+                style={styles.Calendarinput}
+                placeholder="Event Title"
+                placeholderTextColor="#888888" // Set the desired placeholder color
+                value={eventTitle}
+                onChangeText={setEventTitle}
+              />
+              <TextInput
+                style={styles.Calendarinput}
+                placeholder="Event Description"
+                placeholderTextColor="#888888" // Set the desired placeholder color
+                value={eventDescription}
+                onChangeText={setEventDescription}
+                multiline
+              />
+              <TextInput
+                style={styles.Calendarinput}
+                placeholder="Event Place"
+                placeholderTextColor="#888888" // Set the desired placeholder color
+                value={eventPlace}
+                onChangeText={setEventPlace}
+              />
+
+              {/* Calendar for Day Selection */}
+              <Calendar
+                onDayPress={handleDayPress} // Save selected day
+                markedDates={{
+                  [selectedDate]: {
+                    selected: true,
+                    marked: true,
+                    selectedColor: "blue",
+                  },
+                }}
+              />
+
+              {/* Display Selected Date */}
+              <Text style={[styles.dateText, { marginBottom: 10 }]}>
+                Selected Date: {selectedDate || "None"}
+              </Text>
+
+              <Text style={[styles.inputlabel, { marginBottom: 10 }]}>
+                Event Date: {selectedDate || "None"}
+              </Text>
+              <TouchableOpacity
+                style={styles.CalendartimeButton}
+                onPress={() => setIsTimePickerVisible(true)}
+              >
+                <Text style={styles.CalendartimeButtonText}>
+                  Select Time:{" "}
+                  {eventTime.toLocaleTimeString([], {
+                    hour: "2-digit",
+                    minute: "2-digit",
+                    hour12: true,
+                  })}
+                </Text>
+              </TouchableOpacity>
+
+              {/* Time Picker */}
+              {isTimePickerVisible && (
+                <DateTimePicker
+                  value={eventTime}
+                  mode="time" // Time picker mode
+                  is24Hour={false} // Set to true for 24-hour format
+                  display="default"
+                  onChange={handleTimeChange} // Handle time selection
+                />
+              )}
+
+              <TouchableOpacity
+                style={styles.CalendarsaveButton}
+                onPress={saveEvent}
+              >
+                <Text style={styles.CalendarsaveButtonText}>Save Event</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={styles.CalendarcloseButton}
+                onPress={() => setIsEventModalVisible(false)}
+              >
+                <Text style={styles.CalendarcloseButtonText}>X</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </Modal>
+      </Modal>
+
+      <Modal
+        visible={isEventDetailModalVisible}
+        transparent={true}
+        animationType="fade"
+      >
+        <View style={styles.eventDetailModalOverlay}>
+          <View style={styles.eventDetailModal}>
+            {/* Close Button in Top-Right */}
+            <TouchableOpacity
+              style={styles.eventDetailCloseButton}
+              onPress={() => setIsEventDetailModalVisible(false)}
+            >
+              <Text style={styles.eventDetailCloseButtonText}>X</Text>
+            </TouchableOpacity>
+
+            {/* Event Title */}
+            <Text style={styles.eventDetailTitle}>
+              {selectedEventDetails?.title || "No Title"}
+            </Text>
+
+            {/* Event Details */}
+            <Text style={styles.eventDetailText}>
+              Description:{" "}
+              {selectedEventDetails?.description || "No Description"}
+            </Text>
+            <Text style={styles.eventDetailText}>
+              Place: {selectedEventDetails?.place || "No Place"}
+            </Text>
+            <Text style={styles.eventDetailText}>
+              Date: {selectedDate || "No Date Selected"}
+            </Text>
+            <Text style={styles.eventDetailText}>
+              Time: {selectedEventDetails?.time || "No Time Selected"}
+            </Text>
+
+            {/* Event Image */}
+            <Image
+              source={require("../../../assets/Diggy.png")} // Replace with your character image path
+              style={styles.eventCharacterImage}
+            />
+
+            {/* Delete Button in Bottom-Right */}
+            <TouchableOpacity
+              style={styles.eventDetailDeleteButton}
+              onPress={() =>
+                handleDeleteEvent(selectedDate, selectedEventDetails)
+              }
+            >
+              <Text style={styles.eventDetailDeleteButtonText}>Delete</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
+
+      <Modal
+        visible={isTimePickerVisible}
+        transparent={true}
+        animationType="fade"
+      >
+        <View style={styles.timePickerModalOverlay}>
+          <View style={styles.timePickerModal}>
+            {/* Hours Input */}
+            <View style={styles.timePickerRow}>
+              <Text style={styles.timePickerLabel}>Hour:</Text>
+              <TextInput
+                style={styles.timeInput}
+                keyboardType="numeric"
+                maxLength={2} // Restrict input to 2 digits
+                value={selectedHour?.toString() || ""} // Display current hour or empty
+                onChangeText={(value) => {
+                  setSelectedHour(value); // Allow free input
+                }}
+                placeholder="HH"
+                placeholderTextColor="gray"
+              />
+            </View>
+
+            {/* Minutes Input */}
+            <View style={styles.timePickerRow}>
+              <Text style={styles.timePickerLabel}>Minutes:</Text>
+              <TextInput
+                style={styles.timeInput}
+                keyboardType="numeric"
+                maxLength={2} // Restrict input to 2 digits
+                value={selectedMinute?.toString() || ""} // Display current minutes or empty
+                onChangeText={(value) => {
+                  setSelectedMinute(value); // Allow free input
+                }}
+                placeholder="MM"
+                placeholderTextColor="gray"
+              />
+            </View>
+
+            <View style={styles.timePickerRow}>
+              <Text style={styles.timePickerLabel}>AM/PM:</Text>
+              <DropDownPicker
+                open={isDropdownOpen}
+                value={selectedPeriod} // Current value
+                items={[
+                  { label: "AM", value: "AM" },
+                  { label: "PM", value: "PM" },
+                ]}
+                setOpen={setIsDropdownOpen}
+                setValue={setSelectedPeriod}
+                setItems={setDropdownItems}
+                style={styles.timePickerDropdown}
+              />
+            </View>
+
+            {/* Save Button */}
+            <TouchableOpacity
+              style={styles.timePickerSaveButton}
+              onPress={() => handleSaveTime()}
+            >
+              <Text style={styles.timePickerSaveButtonText}>Save</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
+=======
+import React from "react";
+import { View, Text, StyleSheet, ScrollView } from "react-native";
+import { liteClient as algoliasearch } from "algoliasearch/lite";
+import { InstantSearch } from "react-instantsearch-core";
+import { SearchBox } from "../SearchBox";
+import EvilIcons from "@expo/vector-icons/EvilIcons";
+
+const searchClient = algoliasearch("YourApplicationID", "YourSearchOnlyAPIKey");
+
+export default function Home() {
+  return (
+    <ScrollView contentContainerStyle={styles.scrollContainer}>
+      {/* Search bar */}
+      <View style={styles.searchBarContainer}>
+        <View style={styles.searchBarIconContainer}>
+          <EvilIcons name="search" size={24} color="black" />
+        </View>
+        <InstantSearch searchClient={searchClient} indexName="INDEX_NAME">
+          <SearchBox />
+        </InstantSearch>
+      </View>
+>>>>>>> 2817ca2 (Add searchbox at the top of the home page):components/tabs/Home.js
+>>>>>>> e74d933 (feat(home): Add searchbox at the top of the home page):frontend/app/components/tabs/Home.js
     </ScrollView>
   );
 }
@@ -398,6 +709,7 @@ export default function Home() {
 const styles = StyleSheet.create({
   scrollContainer: {
     paddingBottom: 20,
+<<<<<<< HEAD:frontend/app/components/tabs/Home.js
     backgroundColor: "white",
   },
   greenButton: {
@@ -966,4 +1278,30 @@ const styles = StyleSheet.create({
     padding: 10,
     marginLeft: 105, // Add space to move it more to the right
   },
+=======
+    backgroundColor: "#024935",
+  },
+  adSpaceContainer: {
+    width: "343px",
+    height: "56px",
+    backgroundColor: "#16513D",
+    borderRadius: "4px",
+    padding: "8px 16px 8px 12px",
+    gap: "12px",
+  },
+  searchBarContainer: {
+    flex: 1,
+    flexDirection: "row",
+    marginTop: 52,
+    marginLeft: 16,
+    marginRight: 16,
+    backgroundColor: "#fff",
+    borderRadius: 8,
+  },
+  searchBarIconContainer: {
+    flex: 1,
+    justifyContent: "center",
+    paddingLeft: 8,
+  },
+>>>>>>> 2817ca2 (Add searchbox at the top of the home page):components/tabs/Home.js
 });
