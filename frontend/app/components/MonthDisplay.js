d34007a4b538529dd9621b3bd818e4d211a8a326
Aadit Kamat <plunges_abiotic.0q@icloud.com>
2025-04-15 08:55:55 -0400

    feat(utils): Handle date formatting and manipulations using date-fns

--- frontend/app/components/MonthDisplay.js
+++ frontend/app/components/MonthDisplay.js
@@ -7,25 +7,23 @@ import {
   TouchableOpacity,
 } from "react-native";
 import EvilIcons from "@expo/vector-icons/EvilIcons";
-import { printMonthAndYear, daysInMonth, dayNames } from "../util/date";
 import { DayDisplay } from "./DayDisplay";
 import _ from "lodash";
+import { addMonths, subMonths, getDaysInMonth, getDay, format } from "date-fns";
 
 function getMonthDisplayData(date) {
   const year = date.getFullYear();
   const month = date.getMonth();
-  const numOfDays = daysInMonth(year, month);
-
-  return _.map(_.range(1, numOfDays + 1), (dayNum) => {
+  return _.map(_.range(1, getDaysInMonth(date) + 1), (dayNum) => {
     return {
-      dayOfWeek: dayNames[new Date(`${year}-${month}-${dayNum}`).getDay()],
+      dayOfWeek: format(new Date(year, month, dayNum), "iii").toUpperCase(),
       dayNum: dayNum,
     };
   });
 }
 
 export function MonthDisplay() {
-  const [date, setDate] = useState(new Date());
+  const [currentDate, setCurrentDate] = useState(new Date());
   const [selectedId, setSelectId] = useState();
 
   const renderItem = ({ item, index }) => {
@@ -40,17 +38,33 @@ export function MonthDisplay() {
     );
   };
 
+  const handleLeftButtonPress = () => {
+    setCurrentDate(subMonths(currentDate, 1));
+  };
+
+  const handleRightButtonPress = () => {
+    setCurrentDate(addMonths(currentDate, 1));
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.monthToggle}>
         <View style={styles.monthTextContainer}>
-          <Text style={styles.monthText}>{printMonthAndYear(date)}</Text>
+          <Text style={styles.monthText}>
+            {format(currentDate, "LLLL yyyy")}
+          </Text>
         </View>
         <View style={styles.buttonContainer}>
-          <TouchableOpacity style={styles.button}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={handleLeftButtonPress}
+          >
             <EvilIcons name="arrow-left" size={32} color="white" />
           </TouchableOpacity>
-          <TouchableOpacity style={styles.button}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={handleRightButtonPress}
+          >
             <EvilIcons name="arrow-right" size={32} color="white" />
           </TouchableOpacity>
         </View>
@@ -58,7 +72,7 @@ export function MonthDisplay() {
       <View>
         <FlatList
           horizontal={true}
-          data={getMonthDisplayData(date)}
+          data={getMonthDisplayData(currentDate)}
           renderItem={renderItem}
           keyExtractor={(item) => item.dayNum}
           extraData={selectedId}
