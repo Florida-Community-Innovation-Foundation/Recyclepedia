292b5d9bb5654ffb76ba770f42e7ce4a17036bdf
Aadit Kamat <plunges_abiotic.0q@icloud.com>
2025-05-03 09:36:22 -0400

    fix(modal): fix logic to handle minimum date on calendar and selection of days in the period

--- frontend/src/components/events/EventCalendar.jsx
+++ frontend/src/components/events/EventCalendar.jsx
@@ -1,6 +1,7 @@
 import dayjs from "dayjs";
 import { useFormikContext } from "formik";
 import _ from "lodash";
+import { Text } from "react-native";
 import { Calendar } from "react-native-calendars";
 
 export default function EventCalendar({ isCalendarVisible }) {
@@ -12,7 +13,7 @@ export default function EventCalendar({ isCalendarVisible }) {
       let currentDate = dayjs(markedDates[markedDates.length - 1])
         .add(1, "d")
         .format("YYYY-MM-DD");
-      if (dayjs(currentDate).diff(dayjs(values["End Date"], "d")) <= 0) {
+      if (dayjs(currentDate).diff(dayjs(values["End Date"]), "d") <= 0) {
         markedDates.push(currentDate);
         currentDate = dayjs(currentDate).add(1, "d").format("YYYY-MM-DD");
       } else {
@@ -22,23 +23,31 @@ export default function EventCalendar({ isCalendarVisible }) {
     return _.reduce(
       markedDates,
       (result, markedDate) => {
-        result[dayjs(markedDate).format("YYYY-MM-DD")] = {
+        let formatting = {
           color: "#70d7c7",
           textColor: "white",
         };
+        if (dayjs(markedDate).diff(dayjs(values["Start Date"]), "d") === 0) {
+          formatting.startingDay = true;
+        }
+        if (dayjs(markedDate).diff(dayjs(values["End Date"]), "d") === 0) {
+          formatting.endingDay = true;
+        }
+        result[dayjs(markedDate).format("YYYY-MM-DD")] = formatting;
         return result;
       },
       {},
     );
   };
 
-  const handleDayPressed = (day) => {
+  const handleDayPress = (day) => {
     if (dayjs(day.dateString).diff(dayjs(values["Start Date"]), "d") < 0) {
       setFieldValue("Start Date", day.dateString);
     } else if (dayjs(day.dateString).diff(dayjs(values["End Date"]), "d") > 0) {
       setFieldValue("End Date", day.dateString);
     } else {
       setFieldValue("Start Date", day.dateString);
+      setFieldValue("End Date", day.dateString);
     }
   };
 
@@ -46,10 +55,13 @@ export default function EventCalendar({ isCalendarVisible }) {
     return (
       <Calendar
         markingType="period"
+        renderHeader={(date) => {
+          return <Text>{dayjs(date).format("MMM YYYY")}</Text>;
+        }}
         disableAllTouchEventsForDisabledDays={true}
-        minDate={values["Start Date"]}
+        minDate={dayjs().format("YYYY-MM-DD")}
         markedDates={getMarkedDates()}
-        onDayPress={handleDayPressed}
+        onDayPress={handleDayPress}
       />
     );
   }
