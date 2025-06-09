c3d7c0c4ea33784641fcf5b13e4ca38b0cde8ce8
aaditkamat <aadit.kamat@floridainnovation.org>
2025-06-09 15:07:31 +0800

    fix(profile): add more games in seperate screens with a back button to return to profile page

--- frontend/src/app/featuredGameScreen.jsx
+++ frontend/src/app/featuredGameScreen.jsx
@@ -1,10 +1,26 @@
 import FeaturedGame from "../components/games/FeaturedGame";
-import { View } from "react-native";
+import { View, StyleSheet } from "react-native";
+import BackButton from "../components/common/BackButton";
 
 export default function FeaturedGameScreen() {
-    return (
-        <View style={{ flex: 1 }}>
-            <FeaturedGame />
-        </View>
-    );
+  return (
+    <View style={styles.screen}>
+      <BackButton />
+      <View style={styles.featuredGameContainer}>
+        <FeaturedGame />
+      </View>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    backgroundColor: "#024935",
+  },
+  featuredGameContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
--- frontend/src/app/learnGameScreen.jsx
+++ frontend/src/app/learnGameScreen.jsx
@@ -1,10 +1,28 @@
 import LearnGame from "../components/games/LearnGame";
-import { View } from "react-native";
+import BackButton from "../components/common/BackButton";
+import { useLocalSearchParams } from "expo-router";
+import { View, StyleSheet } from "react-native";
 
 export default function LearnGameScreen() {
-    return (
-        <View style={{ flex: 1 }}>
-            <LearnGame />
-        </View>
-    );
+  const { image, title, color, url } = useLocalSearchParams();
+  return (
+    <View style={styles.screen}>
+      <BackButton />
+      <View style={styles.learnGameContainer}>
+        <LearnGame image={image} title={title} color={color} url={url} />
+      </View>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    backgroundColor: "#024935",
+  },
+  learnGameContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
