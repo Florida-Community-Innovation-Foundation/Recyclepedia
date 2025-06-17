00fb08b1c1436e91652d2301efbf96bff261fe7a
Aadit Kamat <plunges_abiotic.0q@icloud.com>
2025-06-17 17:28:14 +0800

    fix(util): update base width while normalizing

--- frontend/src/utils/normalize.js
+++ frontend/src/utils/normalize.js
@@ -1,14 +1,14 @@
 import { Dimensions, PixelRatio } from "react-native";
 
-const baseWidth = 375;
+const baseWidth = 400;
 const baseHeight = 812;
 
-const { width: Screen_Width, height: Screen_Height } = Dimensions.get('window');
+const { width: Screen_Width, height: Screen_Height } = Dimensions.get("window");
 
 const scaleWidth = Screen_Width / baseWidth;
 const scaleHeight = Screen_Height / baseHeight;
 
 export const normalize = (size, based = "width") => {
-    const scale = based === 'height' ? scaleHeight : scaleWidth;
-    return Math.round(PixelRatio.roundToNearestPixel(size * scale));
+  const scale = based === "height" ? scaleHeight : scaleWidth;
+  return Math.round(PixelRatio.roundToNearestPixel(size * scale));
 };
